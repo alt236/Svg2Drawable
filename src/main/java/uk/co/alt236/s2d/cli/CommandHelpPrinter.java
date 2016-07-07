3029af0e61223fcb58d901134fc4b0633a0836ac
Alexandros Schillings <aschillings@gmail.com>
2016-07-07 16:43:34 +0100

    Added sample usage in help

--- src/main/java/uk/co/alt236/s2d/cli/CommandHelpPrinter.java
+++ src/main/java/uk/co/alt236/s2d/cli/CommandHelpPrinter.java
@@ -7,6 +7,7 @@ import uk.co.alt236.s2d.resources.S2DStrings;
 import java.util.ResourceBundle;
 
 public class CommandHelpPrinter {
+    private static final String BINARY_PLACEHOLDER = "##BINARY##";
 
     private final String binaryName;
     private final Options options;
@@ -20,8 +21,10 @@ public class CommandHelpPrinter {
 
         final ResourceBundle labels = new S2DStrings();
 
-        final String header = labels.getString("cli_help_message_header");
-        final String footer = labels.getString("cli_help_message_footer");
+        final String header = labels.getString("cli_help_message_header")
+                .replaceAll(BINARY_PLACEHOLDER, binaryName);
+        final String footer = labels.getString("cli_help_message_footer")
+                .replaceAll(BINARY_PLACEHOLDER, binaryName);
 
         final HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp(binaryName, header, options, footer, true);
